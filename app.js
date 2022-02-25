635a8f993245d2dcbc90043934bffd9498fa45b9
masudrana <masudranaacb@gmail.com>
2022-02-25 22:06:52 +0600

    decusion primitiv non primitiv

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// number
+// string
+// boolean
+// undefinde
+// null
+// symboll
+// primitive data type
+let x=44;
+let y=x;
+// console.log(x,y)
+x=50;
+// console.log(x,y)
+
+// object
+// non primitiv data type
+let a={job:'web development'};
+let b=a;
+console.log(a,b);
+a.job='React Developer';
+console.log(a,b)
+b.job='Frontend Developer';
+console.log(a,b)
